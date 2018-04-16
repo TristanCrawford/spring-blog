bc27f243a2d4c69ba945c6f9556ffb0f4d0b8a0a
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-04-16 16:20:48 -0500

    Dice Roll + Bonus

---
+++ src/main/java/com/codeup/blog/controllers/DiceController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class DiceController {
+
+    private List<Integer> numbers = new ArrayList<>();
+
+    private List<Integer> rollDice(int n) {
+        return Arrays.asList(new Integer[n]).stream().map(i -> (int) (Math.random() * 6 + 1)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/roll-dice")
+    public String index() {
+        numbers = rollDice(20);
+        return "dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String guess(@PathVariable int n, Model model) {
+        model.addAttribute("numbers", numbers);
+        model.addAttribute("guess", n);
+        return "guess";
+    }
+
+}
